138adf055a0f09c05607e55a31498247816ef622
Nummun14 <142012009+Nummun14@users.noreply.github.com>
2023-08-29 17:05:02 +0300

    fixed Differential Drive Constants

--- src/main/java/frc/trigon/robot/subsystems/differentialdrive/DifferentialDriveConstants.java
+++ src/main/java/frc/trigon/robot/subsystems/differentialdrive/DifferentialDriveConstants.java
@@ -1,12 +1,18 @@
 package frc.trigon.robot.subsystems.differentialdrive;
 
+import com.ctre.phoenixpro.configs.TalonFXConfiguration;
+import com.ctre.phoenixpro.hardware.TalonFX;
+import com.ctre.phoenixpro.signals.InvertedValue;
+import com.ctre.phoenixpro.signals.NeutralModeValue;
+import edu.wpi.first.math.controller.PIDController;
+
 public class DifferentialDriveConstants {
 
     private static final int MOTOR1_ID = 1;
     private static final int MOTOR2_ID = 2;
     private static final int MOTOR3_ID = 3;
     private static final int MOTOR4_ID = 4;
-    private static final NeutralModeValue NEUTRAL_MODE_VALUE = NeutralModeValue.Break;
+    private static final NeutralModeValue NEUTRAL_MODE_VALUE = NeutralModeValue.Brake;
     private static final InvertedValue INVERTED_VALUE = InvertedValue.CounterClockwise_Positive;
     static final TalonFX MOTOR1 = new TalonFX(MOTOR1_ID);
     static final TalonFX MOTOR2 = new TalonFX(MOTOR2_ID);
@@ -26,6 +32,9 @@ public class DifferentialDriveConstants {
         config.Audio.BeepOnBoot = false;
         config.MotorOutput.Inverted = INVERTED_VALUE;
         config.MotorOutput.NeutralMode = NEUTRAL_MODE_VALUE;
-        MOTOR.getConfigurator().apply(config);
+        MOTOR1.getConfigurator().apply(config);
+        MOTOR2.getConfigurator().apply(config);
+        MOTOR3.getConfigurator().apply(config);
+        MOTOR4.getConfigurator().apply(config);
     }
 }
